b3c18d9512930719f0885ddcceff990c307eda8a
Luis Macedo <luiscarlos06070607@gmail.com>
2026-05-27 23:03:17 -0300

    Primeira versão do desafio

---
+++ ContaBancaria.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class ContaBancaria {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual o seu nome?");
+        String name = sc.nextLine();
+
+        System.out.println("Qual o tipo da sua conta?");
+        String tipoDaConta = sc.nextLine();
+
+        System.out.println("Informe o seu saldo:");
+        double saldo = sc.nextDouble();
+
+
+        System.out.println("*********************************");
+        System.out.println();
+        System.out.println("Nome:            " + name);
+        System.out.println("Tipo conta:      " + tipoDaConta);
+        System.out.println("Saldo da conta:  " + saldo);
+        System.out.println("*********************************");
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        int operacoes = 0;
+        while (operacoes != 4){
+            System.out.println();
+            System.out.println("Operações:");
+            System.out.println();
+            System.out.println("1 - Consultar saldo:");
+            System.out.println("2 - Receber valor:");
+            System.out.println("3 - Transferir valor:");
+            System.out.println("4 -  Sair:");
+
+            System.out.println();
+            System.out.println("Digite a opção desejada:");
+            operacoes = sc.nextInt();
+
+            System.out.println();
+
+            if (operacoes == 1){
+                System.out.println("Seu saldo atual: R$ " + saldo);
+            }
+            if(operacoes == 2){
+                System.out.println("Informe o valor a receber:");
+                double deposito = sc.nextDouble();
+                saldo += deposito;
+                System.out.println("Saldo atualizado: R$ " + saldo);
+            }
+            if (operacoes == 3){
+                System.out.println("informe o valor que você deseja transferir:");
+                double debito = sc.nextDouble();
+
+                if (debito > saldo){
+                    System.out.println("Não há saldo suficiente para fazer essa transferência.");
+                }else{
+                    saldo -= debito;
+                    System.out.println("Saldo atualizado: R$ " + saldo);
+                }
+            }
+            }
+    }
+}
